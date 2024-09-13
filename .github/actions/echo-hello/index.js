9d863f2449e644722732d686026e0e71debd34b4
Mathiyarasy <157102811+Mathiyarasy@users.noreply.github.com>
2024-09-13 20:59:35 +0530

    Update index.js

--- .github/actions/echo-hello/index.js
+++ .github/actions/echo-hello/index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 
 try {
-  const rts = core.getInput('rts');
+  const rts = process.env.rts;
   console.log(`Hello, ${rts}`);
 } catch (error) {
   core.setFailed(error.message);
